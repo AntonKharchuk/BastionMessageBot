6e2febe6915b595207e17b05746b235dbb632ba0
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-25 19:50:04 +0300

    add IHttpRequestSender implementation

--- NonFirebaseApi/Program.cs
+++ NonFirebaseApi/Program.cs
@@ -1,8 +1,14 @@
+using NonFirebaseApi.Clients;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddTransient<IHttpRequestSender, HttpRequestSender>();
+
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
