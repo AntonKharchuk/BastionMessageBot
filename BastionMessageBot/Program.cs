223148508dc5a3c56a907b1fcd67b9966943658d
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-25 20:35:16 +0300

    remove shit from Progrum

--- BastionMessageBot/Program.cs
+++ BastionMessageBot/Program.cs
@@ -1,21 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BastionMessageBot;
 
-//Bot bot = new Bot();
+Bot bot = new Bot();
 
-//bot.Start();
-
-//Console.ReadKey();
-
-HttpRequestSender httpRequestSender = new HttpRequestSender();
-
-
-var reply = httpRequestSender.SendRequest("https://simple-books-api.glitch.me", "GET").Result;
-
-
-foreach (var header in reply.Headers)
-{
-    Console.WriteLine("{0} - {1}",header.Key, header.Value.First());
-}
+bot.Start();
 
 Console.ReadKey();
