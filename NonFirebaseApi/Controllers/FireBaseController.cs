5e02c2418f31f26e8db615ba30ee38565425b747
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-25 20:33:47 +0300

    Add controller endpoint to add strings to list.txt

--- NonFirebaseApi/Controllers/FireBaseController.cs
+++ NonFirebaseApi/Controllers/FireBaseController.cs
@@ -1,21 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NonFirebaseApi.Clients;
+using NonFirebaseApi.Models;
+using System.Text.Json;
+using System.IO;
+
 
 namespace NonFirebaseApi.Controllers
 {
     public class FireBaseController : ControllerBase
     {
-        [HttpPost("message-tocken")]
+
+        private readonly string _messageTokenListPath;
+
+        private IHttpRequestSender _requestSender;
+
+        public FireBaseController(IHttpRequestSender httpRequestSender)
+        {
+            _requestSender = httpRequestSender;
+            _messageTokenListPath = "D:\\Code\\C#\\ynik\\BastionMessageBot\\NonFirebaseApi\\MessageTokenList.txt";
+        }
+
+
+        [HttpPost("set-message-token")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetRequest([FromQuery] string request)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetRequest([FromBody] string body)
         {
 
-            var result = await _youTubeApiClient.GetSerchByVideoRequest(request);
-
-            if (result == null)
+            if (!string.IsNullOrEmpty(body))
             {
-                return NotFound("Obj not found");
+                try
+                {
+                    MessageToken messageToken = JsonSerializer.Deserialize<MessageToken>(body);
+
+                    SaveTockenToTxt(messageToken.Token);
+
+                    return Ok("Token has been added to List");
+                }
+                catch (Exception)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Error while adding to list");
+                }
             }
+            else
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Token is missing in body");
+            }
+        }
 
         private async Task SaveTockenToTxt(string token)
         {
